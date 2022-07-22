240677d08538d878cf79a7114a747cff7968b8bf
Jeet Bansal <JBansal@slb.com>
2022-07-22 10:17:54 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.io.*;
+  
+class GFG {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("GeeksforGeeks");
+    }
+}
